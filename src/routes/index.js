81d03c540335815f7f6c65d86e78907dadd8dbdc
manuel parada <xkiller838@gmail.com>
2022-08-25 14:54:02 -0500

    ajustando import express

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 const cors = require('cors');
 const morgan = require('morgan');
 const userRouter = require('../routes/user.js');
